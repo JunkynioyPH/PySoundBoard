a8ff02fa14af8199920be2a166e5d395d10cb87a
Junkynioy <virgil_bravo@yahoo.com>
2022-02-01 21:44:05 +0800

    Initial Commit

---
+++ AudioDef.py
@@ -0,0 +1,136 @@
+import pygame
+import os
+import time
+
+LoopState = 0
+LoopTextState = "No Loop"
+
+def ToggleLoop():
+    global LoopState, LoopTextState
+    if LoopState == 0:
+        LoopTextState = "Looping"
+        LoopState = -1
+    else:
+        LoopTextState = "No Loop"
+        LoopState = 0
+
+def Play(AudioFile):
+    global LoopState, LoopTextState
+    pygame.mixer.music.unload()
+    pygame.mixer.music.load(AudioFile)
+    pygame.mixer.music.play(loops=LoopState)
+    pygame.init()
+
+def SmashBroDrillRemix():
+    Play(".\SoundFiles\\smashbrodrillremix.mp3")
+
+def VineBoom():
+    Play(".\SoundFiles\\vineboom.mp3")
+
+def DreamTranceMusic():
+    Play(".\SoundFiles\\dreamtrancemusic.mp3")
+
+def DSoulsDeath():
+    Play(".\SoundFiles\\dsoulsdeath.mp3")
+
+def RobloxOof():
+    Play(".\SoundFiles\\robloxoof.mp3")
+
+def SteveOof():
+    Play(".\SoundFiles\\steveoof.mp3")
+
+def SamsungStartUp():
+    Play(".\SoundFiles\\samsungstartup.mp3")
+
+def UltraInstinct():
+    Play(".\SoundFiles\\ultrainstinct.mp3")
+
+def GiornoThemePiano():
+    Play(".\Soundfiles\\giornothemepiano.mp3")
+
+def SickoModeWaaah():
+    Play(".\SoundFiles\\sickomodewaaah.mp3")
+
+def DSoulsBossMusic():
+    Play(".\SoundFiles\\dsoulsbossmusic.mp3")
+
+def ArabicRingtone():
+    Play(".\SoundFiles\\arabicringtone.mp3")
+
+def IndianMusicMeme():
+    Play(".\SoundFiles\\panjabimc.mp3")
+
+def SusBodyReported():
+    Play(".\SoundFiles\\susbodyreported.mp3")
+
+def EmotionalDamage():
+    Play(".\SoundFiles\\emotionaldamage.mp3")
+
+def JebNooo():
+    Play(".\SoundFiles\\jebnooo.mp3")
+
+def Bruh():
+    Play('.\SoundFiles\\bruh.mp3')
+
+def SusImposterRole():
+    Play(".\SoundFiles\\susimposterrole.mp3")
+
+def SadHarmonicaEar():
+    Play(".\SoundFiles\\sadharmonicaEar.mp3")
+
+def SadHarmonica():
+    Play(".\SoundFiles\\sadharmonica.mp3")
+
+def DJAirhorn():
+    Play(".\SoundFiles\\djairhorn.mp3")
+
+def ISendU2Jesus():
+    Play(".\SoundFiles\\isendu2jesus.mp3")
+
+def SmileDogMeme():
+    Play(".\SoundFiles\\smiledogmeme.mp3")
+
+def Helicopterx2():
+    Play(".\SoundFiles\\helicopterx2.mp3")
+
+def WHATTHEFUCK():
+    Play(".\SoundFiles\\wtf.ogg")
+
+def WHAT():
+    Play(".\SoundFiles\\wat.ogg")
+
+def ToBeContinued():
+    Play(".\SoundFiles\\tbc.mp3")
+
+def PHub():
+    Play(".\SoundFiles\\phub.mp3")
+
+def ThunderStorm():
+    Play(".\SoundFiles\\thunderstorm.mp3")
+
+def KahootLobby():
+    Play(".\SoundFiles\\kahootlobby.mp3")
+
+def HeartFlatline():
+    Play(".\SoundFiles\\heartflatline.mp3")
+
+def WhatHow_Meme():
+    Play(".\SoundFiles\\whathowmeme.mp3")
+
+def GTAWasted():
+    Play(".\SoundFiles\\gtawasted.mp3")
+
+def BFGDivision():
+    Play(".\SoundFiles\\bfgdivision.mp3")
+
+def WinXPShutDown():
+    Play(".\SoundFiles\\winxpshutdown.wav")
+
+def WinXPStartup():
+    Play(".\SoundFiles\\winxpstartup.wav")
+
+def WinXPCritStop():
+    Play(".\SoundFiles\\winxpcritstop.wav")
+
+def WinXPError():
+    Play(".\SoundFiles\\winxperror.wav")
---
+++ SoundBoard_Main.py
@@ -0,0 +1,295 @@
+from tkinter import *
+from tkinter import ttk
+from pathlib import Path
+
+import pygame
+import AudioDef
+import json
+import os
+
+# Preliminary preparations
+os.system("title SoundBoard Backend")
+root = Tk()
+root.title("SoundBoard GUI")
+
+# Cointainers
+header = ttk.Frame(root, relief=RAISED, borderwidth=8)
+header.grid(padx=5, pady=5, column=0, row=0, sticky=(N, W, E, S))
+
+mframe = ttk.Frame(root, relief=GROOVE, borderwidth=2)
+mframe.grid(padx=10, pady=10, column=0, row=1, sticky=(N, W, E, S))
+
+controls = ttk.Frame(mframe, relief=SUNKEN, borderwidth=2)
+controls.grid(padx=5, pady=5, column=0, row=0, sticky=(N, W, E, S))
+
+soundboard = ttk.Frame(mframe, relief=SUNKEN, borderwidth=2)
+soundboard.grid(padx=5, pady=5, column=1, row=0, sticky=(N, W, E, S))
+
+soundbuttons = ttk.Frame(soundboard, relief=SUNKEN, borderwidth=2)
+soundbuttons.grid(padx=5, pady=5, column=1, row=1, sticky=(N, W, E, S))
+
+controllabel = ttk.Frame(controls, relief=RAISED, borderwidth=2)
+controllabel.grid(column=1, row=1, sticky=(N, W, E, S))
+
+soundlabel = ttk.Frame(soundboard, relief=RAISED, borderwidth=2)
+soundlabel.grid(column=1, row=0, sticky=(N, W, E, S))
+
+# Controls
+def Pause():
+    pygame.mixer.music.pause()
+
+def Resume():
+    pygame.mixer.music.unpause()
+    print("If you have stopped audio, and it's still incrementing the 'Song Position' above 'Toggle Loop'\nYou can Ignore this issue.")
+
+def Stop():
+    pygame.mixer.music.fadeout(250)
+
+# Load Settings
+def CheckPath2Settings():
+    global Settings
+    if Path("Settings.json").exists() == True:
+        try:
+            with open('Settings.json','r') as SettingsValue:
+                SettingsData = SettingsValue.read()
+            Settings = json.loads(SettingsData)
+        except Exception as Err:
+            print("\nError Occured:\n"+str(Err)+"\n")
+            os.remove("Settings.json")
+            print("settings.json is being reset")
+            CheckPath2Settings()
+            print("settings.json reset complete")
+    else:
+        x = {"AudioDevice":"CABLE Input (VB-Audio Virtual Cable)","Volume":10,"Splash":1}
+        DefSettingsDump = open("Settings.json","a")
+        print("settings.json is made")
+        DefSettingsDump.write(json.dumps(x))
+        print("settings.json is written to")
+        DefSettingsDump.close()
+        print("settings.json is closed")
+        CheckPath2Settings()
+        print("Rerun CheckPath2Settings")
+
+# Update Settings
+def UpdateSettings(Variable,Value):
+    Settings[Variable] = Value
+    UpdateSettings = open("Settings.json","w")
+    print("settings.json is opened")
+    UpdateSettings.write(json.dumps(Settings))
+    print("settings.json is updated")
+    UpdateSettings.close()
+    print(Settings)
+
+CheckPath2Settings()
+
+# Show Current Settings
+print("\nSettings")
+print(Settings)
+
+# initialize Values
+DefaultValSettings = ["CABLE Input (VB-Audio Virtual Cable)",10]
+
+AudioDevice = StringVar()
+AudioDevice.set(Settings["AudioDevice"])
+
+Vol = StringVar()
+Vol.set(Settings["Volume"])
+
+SongPos = StringVar()
+SongPos.set('0')
+
+LoopState = StringVar()
+
+AnimatedBarText1 = StringVar()
+AnimatedBarText2 = StringVar()
+
+TextContent1 = ""
+TextContent2 = ""
+
+# Since I cant fucking use TTK's Progress Bar and find a working all-rounder AudioVisualizer
+# I made my fucking own Visualiser that shows that something is happening alonng with the
+# song Position counter
+#
+#Its only purpose is to fill out that space that is existent at the left of the window
+def AnimatedBar():
+    global TextContent1, TextContent2
+    SongPosition = float(pygame.mixer.music.get_pos()/1000)
+    if SongPosition > 0:
+        #print('pos > 0')
+        TextContent1 += "|"
+        AnimatedBarText1.set(TextContent1)
+        AnimatedBarText2.set(TextContent2)
+        if len(TextContent1) > 13:
+            TextContent1 = ""
+            TextContent2 += "|"
+            if len(TextContent2) > 14:
+                TextContent2 = ""
+                # i could prolly add somethinng here, i just dont know
+    else:
+        #print('pos < 0')
+        TextContent1 = ""
+        TextContent2 = ""
+        AnimatedBarText1.set(TextContent1)
+        AnimatedBarText2.set(TextContent2)
+
+def live_update():
+    try:
+        SongPos.set(str(pygame.mixer.music.get_pos()/1000)+"s")
+        LoopState.set(AudioDef.LoopTextState)
+        root.after(250, AnimatedBar)
+        root.after(100, live_update)
+    except Exception as Err:
+        print(Err)
+
+def stop_live_update():
+    root.after_cancel(live_update)
+    root.after_cancel(AnimatedBar)
+
+def ChangeAudioDevice():
+    Device = AudioDevice.get()
+    try:
+        stop_live_update()
+        pygame.quit()
+        pygame.mixer.pre_init(devicename=Device)
+        pygame.mixer.init()
+        pygame.mixer.music.set_volume(0.1)
+        live_update()
+        UpdateSettings("AudioDevice",Device)
+        print("\n"+AudioDevice.get()+" Found!\nSuccessfully Bound to Device!")
+        AudioDef.Play(".\SoundFiles\start.ogg")
+    except Exception as err:
+        print("\nThere's "+str(err)+"\n"+AudioDevice.get())
+        AudioDevice.set(Settings["AudioDevice"])
+        ChangeAudioDevice()
+
+def SetVol():
+    try:
+        Volume = float(Vol.get())/100
+        if Volume >= 1:
+            pygame.mixer.music.set_volume(Volume)
+            print("\nVolume is now set to '100%'!")
+            Vol.set(pygame.mixer.music.get_volume()*100)
+            UpdateSettings("Volume",Vol.get())
+        else:
+            pygame.mixer.music.set_volume(Volume)
+            print("\nVolume changed to '"+Vol.get()+"%'!")
+            Vol.set(pygame.mixer.music.get_volume()*100)
+            UpdateSettings("Volume",Vol.get())
+    except Exception as err:
+        print("\n'"+str(Vol.get())+"' is not a Valid Number between 0-100!")
+        Vol.set(pygame.mixer.music.get_volume()*100)
+
+# Show First-Time Execution
+if int(Settings["Splash"]) == 1:
+    os.system('python Splash.py')
+    UpdateSettings("Splash",0)
+
+# Start audio system
+try:
+    pygame.mixer.pre_init(devicename=AudioDevice.get())
+    pygame.mixer.init()
+    pygame.mixer.music.set_volume(float(Vol.get())/100)
+    AudioDef.Play(".\SoundFiles\start.ogg")
+except Exception as Err:
+    print("=====================================")
+    print("Fatal Error During PygameMixerInit : ")
+    print(Err)
+    print("=====================================")
+    print(Settings)
+    print("AudioDevice & Volume Settings was Reset due to one of the settings containining Errors.\nSorry for the inconvenience")
+    UpdateSettings("AudioDevice",DefaultValSettings[0])
+    UpdateSettings("Volume",DefaultValSettings[1])
+# initialize GUI placement
+btn = ttk.Button
+lb = ttk.Label
+R = 2
+
+ttk.Style().configure(".",font=('UD Digi Kyokasho N-B', 10))
+
+# Author and Titles
+lb(header, text="Soundboard written in Python! - By: Junkynioy#2408").grid(column=3, row=1,sticky=N)
+
+# Change Audio Device
+lb(header, text="       ").grid(column=1, row=2,sticky=E)
+lb(header, text="AudioDevice").grid(column=2, row=2,sticky=E)
+SetAudDev_entry = ttk.Entry(header, width=75, textvariable=AudioDevice)
+SetAudDev_entry.grid(column=3,row=2,sticky=(N,S,E,W))
+btn(header,text="Set Device",command=ChangeAudioDevice).grid(column=4,row=2,sticky=(N,S,E,W))
+
+# Labels and Info
+lb(controllabel, text="Controls").grid(column=1, row=1,sticky=(N,S,E,W))
+lb(soundlabel, text="Sound Board").grid(column=1, row=1,sticky=(N,S,E,W))
+
+
+# Audio Controls
+btn(controls,text="Stop Playback",command=Stop).grid(column=1,row=R,sticky=(N,S,E,W))
+btn(controls,text="Pause Playback",command=Pause).grid(column=1,row=R+1,sticky=(N,S,E,W))
+btn(controls,text="Resume Playback",command=Resume).grid(column=1,row=R+2,sticky=(N,S,E,W))
+SetVol_entry = ttk.Entry(controls, width=7, textvariable=Vol)
+SetVol_entry.grid(column=1,row=R+3,sticky=(W, E))
+btn(controls,text="^ SetVolume",command=SetVol).grid(column=1,row=R+4,sticky=(N,S,E,W))
+lb(controls, textvariable=SongPos).grid(column=1, row=R+5,sticky=(N,S))
+btn(controls,text="Toggle Loop",command=AudioDef.ToggleLoop).grid(column=1,row=R+6,sticky=(N,S,E,W))
+lb(controls, text="Next played has").grid(column=1, row=R+7,sticky=S)
+lb(controls, textvariable=LoopState).grid(column=1, row=R+8,sticky=N)
+lb(controls, textvariable=AnimatedBarText1).grid(column=1, row=R+9,sticky=N)
+lb(controls, text="]").grid(column=1, row=R+9,sticky=E)
+lb(controls, text="[").grid(column=1, row=R+9,sticky=W)
+lb(controls, textvariable=AnimatedBarText2).grid(column=1, row=R+10,sticky=N)
+lb(controls, text="]").grid(column=1, row=R+10,sticky=E)
+lb(controls, text="[").grid(column=1, row=R+10,sticky=W)
+
+# Audio Files (Col 1)
+btn(soundbuttons, text="SmashBroDrillRemix",command=AudioDef.SmashBroDrillRemix).grid(column=1,row=R,sticky=(N,S,E,W))
+btn(soundbuttons, text="UltraInstinct",command=AudioDef.UltraInstinct).grid(column=1,row=R+1,sticky=(N,S,E,W))
+btn(soundbuttons, text="DSoulsBossMusic",command=AudioDef.DSoulsBossMusic).grid(column=1,row=R+2,sticky=(N,S,E,W))
+btn(soundbuttons, text="DSoulsDeath",command=AudioDef.DSoulsDeath).grid(column=1,row=R+3,sticky=(N,S,E,W))
+btn(soundbuttons, text="ArabicRingtone",command=AudioDef.ArabicRingtone).grid(column=1,row=R+4,sticky=(N,S,E,W))
+btn(soundbuttons, text="SamsungStartUp",command=AudioDef.SamsungStartUp).grid(column=1,row=R+5,sticky=(N,S,E,W))
+btn(soundbuttons, text="VineBoom",command=AudioDef.VineBoom).grid(column=1,row=R+6,sticky=(N,S,E,W))
+btn(soundbuttons, text="RobloxOof",command=AudioDef.RobloxOof).grid(column=1,row=R+7,sticky=(N,S,E,W))
+btn(soundbuttons, text="SteveOof",command=AudioDef.SteveOof).grid(column=1,row=R+8,sticky=(N,S,E,W))
+
+# Audio Files (Col 2)
+btn(soundbuttons,  text="KahootLobby", command=AudioDef.KahootLobby).grid(column=2, row=R,sticky=(N,S,E,W))
+btn(soundbuttons,  text="ToBeContinued", command=AudioDef.ToBeContinued).grid(column=2, row=R+1,sticky=(N,S,E,W))
+btn(soundbuttons,  text="SusImposterRole", command=AudioDef.SusImposterRole).grid(column=2, row=R+2,sticky=(N,S,E,W))
+btn(soundbuttons,  text="SusBodyReported", command=AudioDef.SusBodyReported).grid(column=2, row=R+3,sticky=(N,S,E,W))
+btn(soundbuttons,  text="EmotionalDamage",command=AudioDef.EmotionalDamage).grid(column=2,row=R+4,sticky=(N,S,E,W))
+btn(soundbuttons,  text="ISendU2Jesus",command=AudioDef.ISendU2Jesus).grid(column=2,row=R+5,sticky=(N,S,E,W))
+btn(soundbuttons,  text="HeartFlatline", command=AudioDef.HeartFlatline).grid(column=2, row=R+6,sticky=(N,S,E,W))
+btn(soundbuttons,  text="WHATTHEFUCK", command=AudioDef.WHATTHEFUCK).grid(column=2, row=R+7,sticky=(N,S,E,W))
+btn(soundbuttons,  text="WHAT", command=AudioDef.WHAT).grid(column=2, row=R+8,sticky=(N,S,E,W))
+
+# Audio Files (Col 3)
+btn(soundbuttons,  text="ThunderStorm", command=AudioDef.ThunderStorm).grid(column=3, row=R,sticky=(N,S,E,W))
+btn(soundbuttons,  text="BFGDivision", command=AudioDef.BFGDivision).grid(column=3, row=R+1,sticky=(N,S,E,W))
+btn(soundbuttons,  text="SmileDogMeme", command=AudioDef.SmileDogMeme).grid(column=3, row=R+2,sticky=(N,S,E,W))
+btn(soundbuttons,  text="Helicopter*2", command=AudioDef.Helicopterx2).grid(column=3, row=R+3,sticky=(N,S,E,W))
+btn(soundbuttons,  text="WinXPShutDown", command=AudioDef.WinXPShutDown).grid(column=3, row=R+4,sticky=(N,S,E,W))
+btn(soundbuttons,  text="WinXPStartup", command=AudioDef.WinXPStartup).grid(column=3, row=R+5,sticky=(N,S,E,W))
+btn(soundbuttons,  text="WinXPCritStop", command=AudioDef.WinXPCritStop).grid(column=3, row=R+6,sticky=(N,S,E,W))
+btn(soundbuttons,  text="WinXPError", command=AudioDef.WinXPError).grid(column=3, row=R+7,sticky=(N,S,E,W))
+btn(soundbuttons,  text="SadHarmonica(!!!)", command=AudioDef.SadHarmonicaEar).grid(column=3, row=R+8,sticky=(N,S,E,W))
+
+# AudioFiles (Col 4)
+btn(soundbuttons,  text="PHub", command=AudioDef.PHub).grid(column=4, row=R,sticky=(N,S,E,W))
+btn(soundbuttons,  text="GTAWasted", command=AudioDef.GTAWasted).grid(column=4, row=R+1,sticky=(N,S,E,W))
+btn(soundbuttons,  text="GiornoThemePiano", command=AudioDef.GiornoThemePiano).grid(column=4, row=R+2,sticky=(N,S,E,W))
+btn(soundbuttons,  text="SickoModeWaaah", command=AudioDef.SickoModeWaaah).grid(column=4, row=R+3,sticky=(N,S,E,W))
+btn(soundbuttons,  text="DreamTranceMusic", command=AudioDef.DreamTranceMusic).grid(column=4, row=R+4,sticky=(N,S,E,W))
+btn(soundbuttons,  text="IndianMusicMeme", command=AudioDef.IndianMusicMeme).grid(column=4, row=R+5,sticky=(N,S,E,W))
+
+btn(soundbuttons,  text="DJAirhorn", command=AudioDef.DJAirhorn).grid(column=4, row=R+6,sticky=(N,S,E,W))
+btn(soundbuttons,  text="WhatHow_Meme", command=AudioDef.WhatHow_Meme).grid(column=4, row=R+7,sticky=(N,S,E,W))
+btn(soundbuttons,  text="SadHarmonica", command=AudioDef.SadHarmonica).grid(column=4, row=R+8,sticky=(N,S,E,W))
+
+# AudioFiles (Col 5)
+btn(soundbuttons,  text="Bruh", command=AudioDef.Bruh).grid(column=5, row=R,sticky=(N,S,E,W))
+btn(soundbuttons,  text="JebNooo", command=AudioDef.JebNooo).grid(column=5, row=R+1,sticky=(N,S,E,W))
+
+# Show GUI and Enable live_update
+root.resizable(width=False, height=False)
+live_update()
+root.mainloop()
+pygame.quit()
