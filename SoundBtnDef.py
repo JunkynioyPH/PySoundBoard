3df214edd93f6a5a8c95eea32d7e997f63bb18e4
Junkynioy <virgil_bravo@yahoo.com>
2022-07-23 19:01:18 +0800

    fixing reverting again

--- SoundBtnDef.py
+++ SoundBtnDef.py
@@ -1,25 +1,95 @@
 import AudioDef as AD
-# from AudioDef import *
-import time
 
-# Structure ["DisplayName,AD.DisplayName"]
-
-ComDispName = []
-
-for Files in AD.AudioFilesIndex:
-    x, y = [], ""
-    for letter in Files:
-        if letter == ".":
-            break
-        else:
-            x += letter
-    y += "".join(x)
-    print(y)
-
-    ComDispName += [f"{y}","a"]
-
-time.sleep(1)
-
-print(ComDispName)
-
-time.sleep(5)
+ComDispName = [
+["SmashBroDrillRemix",AD.SmashBroDrillRemix],
+["UltraInstinct",AD.UltraInstinct],
+["BFGDivision",AD.BFGDivision],
+["DSoulsBossMusic",AD.DSoulsBossMusic],
+["DSoulsDeath",AD.DSoulsDeath],
+["ArabicRingtone",AD.ArabicRingtone],
+["SamsungStartUp",AD.SamsungStartUp],
+["OsmanthusWine",AD.OsmanthusWine],
+["OzMenThusWaYn",AD.OsManThuSWinE],
+["ThunderStorm",AD.ThunderStorm],
+["KahootLobby",AD.KahootLobby],
+["ToBeContinued",AD.ToBeContinued],
+["DreamTranceMusic",AD.DreamTranceMusic],
+["GiornoThemePiano",AD.GiornoThemePiano],
+["SusImposterRole",AD.SusImposterRole],
+["SusBodyReported",AD.SusBodyReported],
+["SusRemix",AD.SusRemix],
+["JPSusRemix",AD.JPSusRemix],
+["EmotionalDamage",AD.EmotionalDamage],
+["ISendU2Jesus",AD.ISendU2Jesus],
+["WHATTHEFUCK",AD.WHATTHEFUCK],
+["YouWhat",AD.YouWhat],
+["WHAT",AD.WHAT],
+["Bonk",AD.Bonk],
+["AnimeBonk",AD.AnimeBonk],
+["Bruh",AD.Bruh],
+["MutaharLaugh",AD.MutaharLaugh],
+["CryEmojiVanish",AD.CryEmojiVanish],
+["HiPitchLaugh",AD.HiPitchLaugh],
+["VineBoom",AD.VineBoom],
+["TacoBellBoom",AD.TacoBellBoom],
+["RobloxOof",AD.RobloxOof],
+["SteveOof",AD.SteveOof],
+["JebNooo",AD.JebNooo],
+["McCaveNoise",AD.McCaveNoise],
+["McChallengeGet",AD.McChallengeGet],
+["SmileDogMeme",AD.SmileDogMeme],
+["Helicopter*2",AD.Helicopterx2],
+["GTAWasted",AD.GTAWasted],
+["SynthBladeRun2048",AD.SynthBladeRun2048],
+["WhatHow_Meme",AD.WhatHow_Meme],
+["BeserkST4Gatsu",AD.BeserkST4Gatsu],
+["SadBleachOST19",AD.SadBleachOST19],
+["SadNarutoOST13",AD.SadNarutoOST13],
+["SadHarmonica(!!!)",AD.SadHarmonicaEar],
+["SadHarmonica",AD.SadHarmonica],
+["RickRoll",AD.RickRoll],
+["JPRickroll",AD.JPRickRoll],
+["ReZeroGigguk",AD.ReZeroGigguk],
+["Unravel",AD.Unravel],
+["OhHarderDaddy",AD.OhHarderDaddy],
+["AnimeGirlAH",AD.AnimeGirlAH],
+["Yamete~",AD.Yamete],
+["YameteKudasai~",AD.YameteKudasai],
+["ZoneAnkha",AD.ZoneAnkha],
+["HeartFlatline",AD.HeartFlatline],
+["LovePakistan",AD.LovePakistan],
+["FBIOpenUp",AD.FBIOpenUp],
+["THXIntro",AD.THXIntro],
+["SnoreAAAH",AD.SnoreAAAH],
+["FNAF2Ambience",AD.FNAF2Ambience],
+["DJAirhorn",AD.DJAirhorn],
+["WidePutin",AD.WidePutin],
+["Shawty",AD.Shawty],
+["YTFULying",AD.YTFULying],
+["SpidermanDunked",AD.SpidermanDunked],
+["DarudeSandstorm",AD.DarudeSandstorm],
+["SickoModeWaaah",AD.SickoModeWaaah],
+["GangstaParadise",AD.GangstaParadise],
+["SasukeTheme",AD.SasukeTheme],
+["ChadMan",AD.ChadMan],
+["SigmaMindset",AD.SigmaMindset],
+["SigmaMindSlow",AD.SigmaMindSlow],
+["MissTheRage",AD.MissTheRage],
+["BoyWithUkeToxic",AD.BoyWithUkeToxic],
+["SheKnows",AD.SheKnows],
+["BSBlazing",AD.BSBlazing],
+["IWonderHowWhy",AD.IWonderHowWhy],
+["OkIPullUp",AD.OkIPullUp],
+["SpidermanBSuit",AD.SpidermanBSuit],
+["FrenchAccordion",AD.FrenchAccordion],
+["USSRAnthem",AD.USSRAnthem],
+["LionSleepsTonight",AD.LionSleepsTonight],
+["PHub",AD.PHub],
+["WinXPShutDown",AD.WinXPShutDown],
+["WinXPStartup",AD.WinXPStartup],
+["WinXPCritStop",AD.WinXPCritStop],
+["WinXPError",AD.WinXPError],
+["C_mZone",AD.C_mZone],
+["C_mThrone",AD.C_mThrone],
+["C_mWontLast",AD.C_mWontLast]
+]
