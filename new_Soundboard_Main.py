31faf5a6628ca17132645c1095559db711f35ba6
Junkynioy <virgil_bravo@yahoo.com>
2022-12-26 17:45:21 +0800

    test commit

--- new_Soundboard_Main.py
+++ new_Soundboard_Main.py
@@ -5,8 +5,3 @@ from pygame import mixer
 import time, json, os
 import SoundBtnDef as SD
 import AudioDef as AD
-
-# can you press the X button on this tab ?
-
-if __name__ == "__main__":
-    print("Stub")
