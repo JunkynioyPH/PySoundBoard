57e7f550ab855c84ed48b2cc003a4a8ed7b978ed
Junkynioy <virgil_bravo@yahoo.com>
2025-06-03 03:14:53 +0800

    i thought i removed this...????

--- ListAudioDevices.py
+++
@@ -1,8 +0,0 @@
-import sounddevice
-
-devs = sounddevice.query_devices()
-print(devs) # Shows current output and input as well with "<" abd ">" tokens
-
-for dev in devs:
-    print(dev['name'])
-input("\n\nPress any key(s) to exit")
