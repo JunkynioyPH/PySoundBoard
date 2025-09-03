fdacdcbefa66f09fbe21cf093e5030e73b0c341d
Junkynioy <33281422+JunkynioyPH@users.noreply.github.com>
2025-09-04 04:29:52 +0800

    Pulling Latest Stable (#3)
    * Added Workaround for my Linux Pipewire problems.
    
    I was having problems with frontpanel mic --> headset while having my soundboard --> both my mic mix and headset in Pipewire.
    The Soundboard inadvertantly "connected" the mic and front-panel, which causes some weird stuttering and crunchy audio from front-panel while I have Firefox playing audio.
    
    so I added a linux-exclusive patch.
    Added hostAudioPool, which functions the same as audioPool['audio'], but uses the default audio device.
    This way you can still hear the sound while also still playing through the microphone without manual Pipewire re-routing.
    
    * duplicate entries, 'sound' multimode fix, pretty CLI.
    
    Originally AudioSystem stops creating duplicate entries, which led to, same filename different file extention, files to not be loaded as it's 'technically' already loaded based on the filename (not filename.extension).
    AudioSystem now allows duplicate file names to be loaded but with a unique number prefixed to the duplicated entry with the attatched filepath to the file with different file extension.
    This change is also reflected on the PySoundboard GenerateSoundIndex to properly reference the duplicated entry.
    
    SoundEffect fix for multimode/singlemode, it broke when i was implementing the feature to AudioMedia.
    
    The CLI output is now pretty-fied. Which I will probably regret later but im pushing it anyways :))))))))))

--- AudioSystem_PyQt6.py
+++ AudioSystem_PyQt6.py
@@ -1,14 +1,17 @@
 from PyQt6.QtMultimedia import *
 from PyQt6.QtCore import QUrl, QTimer
 from xpfpath import xpfp
-import time, os
+import time, os, rich
+from rich import pretty
+pretty.install()
+
 
 # main
 class AudioManager():
     def __init__(self, device:QAudioDevice, audioVolume:int=14, soundVolume:int=14, musicPoolSize:int=8):
         """The Main Class which holds everything about the Audio System"""
-        print(f"\nSupported MIME Types [QSoundEffect]:\n{QSoundEffect.supportedMimeTypes()}\n\nDetected AudioOutputs:\n{[Device.description() for Device in QMediaDevices.audioOutputs()]}\n")
-        print(f'Using Device: {device.description() if device is not None else 'System Default'}\n')
+        rich.print(f"\nSupported MIME Types [QSoundEffect]:\n{QSoundEffect.supportedMimeTypes()}\n\nDetected AudioOutputs:\n{[Device.description() for Device in QMediaDevices.audioOutputs()]}\n")
+        rich.print(f'Using Device: {device.description() if device is not None else 'System Default'}\n')
         
         self.settings:dict[str, QAudioDevice|dict[str, int]] = {"device":device,"volume":{'audio':audioVolume,'sound':soundVolume}}
         
@@ -28,19 +31,19 @@ class AudioManager():
         pass
              
     def status(self, cli:bool=True) -> None|str:
-        """Prints out the current Status of AudioManager"""
+        """rich.prints out the current Status of AudioManager"""
         status:str = f"...Index..:\n   Audio: {self.audioIndex['audio']}\n   Sound: {self.audioIndex['sound']}\n\nAudioPool.:\n   Audio:\n{self.audioPool['audio']}\n\n   Sound:\n{self.audioPool['sound']}"
         if cli:
-            print('++ [AudioManager] ++')
-            print(status)
-            print('++ -------------- ++')
+            rich.print('++ [AudioManager] ++')
+            rich.print(status)
+            rich.print('++ -------------- ++')
         else:
             return f'AudioPool.:\n   Audio:\n{self.audioPool['audio']}\n\n   Sound:\n{self.audioPool['sound']}'
         
     def setVolume(self, type:str, vol:int):
         # Check if type exist
         if type.lower() not in ('audio','sound'):
-            return print("*Unknown Type*")
+            return rich.print("[yellow b]*Unknown Type*[/yellow b]")
         
         # update AudioManager Setting
         self.settings['volume'][type.lower()] = vol
@@ -66,7 +69,7 @@ class AudioManager():
         
     def load(self, type:str, path:str):
         audioName:str = os.path.splitext(os.path.basename(path))[0]
-        print(f"[AudioManager] Load: ({type}) '{audioName}' <{path}> ", end='')
+        rich.print(f"[AudioManager] [green]Load:[/green] ({type}) '{audioName}' [magenta b]<{path}>[/magenta b] ", end='')
         
         ## Normalise path to have ' ./ , .\\ ' prefix
         ## In windows, this check will fail and duplicate " .\\ "
@@ -76,63 +79,64 @@ class AudioManager():
         
         # Check if type exist
         if type.lower() not in ('audio','sound'):
-            return print("*Unknown Type*")
-        # Check if key exist in dict, say it's a duplicate if it is
+            return rich.print("[yellow b]*Unknown Type*[/yellow b]")
+        # Check if key exist in dict, say it's a duplicate if it is, and append disriminator
         if  self.audioIndex[type.lower()].get(audioName):
-            return print(f'*Duplicate*')
+            audioName = f"{len(self.audioIndex['audio'])^len(audioName)}_{audioName}"
+            rich.print(f'as [blue]<{audioName}>[/blue] ', end='')
         
         # else, make key
         if type.lower() == 'audio':
             self.audioIndex['audio'][audioName] = path
-            print(f"*Loaded*")
+            rich.print(f"[green b]*Loaded*[/green b]")
         else:
             self.audioIndex['sound'][audioName] = path
-            print(f"*Loaded*")
+            rich.print(f"[green b]*Loaded*[/green b]")
                 
     def unload(self, type:str, item:str):
-        print(f"[AudioManager] Unload: ({type}) <{item}> ", end='')
+        rich.print(f"[AudioManager] [red]Unload:[/red] ({type}) [magenta b]<{item}>[/magenta b] ", end='')
         # Check if type exist
         if type.lower() not in ('audio','sound'):
-            return print("*Unknown Type*")
+            return rich.print("[yellow b]*Unknown Type*[/yellow b]")
         # If it exists, ever. if not reply already unloaded
         if not self.audioIndex.get(type.lower()) and not self.audioIndex[type.lower()].get(item):
-            return print(f"*Already Unloaded*")
+            return rich.print(f"[red b]*Already Unloaded*[/red b]")
 
         # else, unload
         if type.lower() == 'audio':
             self.audioIndex['audio'].pop(item)
-            print(f"*Unloaded*")
+            rich.print(f"[red b]*Unloaded*[/red b]")
         else:
             self.audioIndex['sound'].pop(item)
-            print(f"*Unloaded*")
+            rich.print(f"[red b]*Unloaded*[/red b]")
     
     def toggleState(self, type:str, mode:str):
-        print(f"[AudioManager] ToggleState: ({type}) '{mode}' ", end='')
+        rich.print(f"[AudioManager] ToggleState: ({type}) '{mode}' ", end='')
         if type.lower() not in ('audio','sound'):
-            return print('*Unknown Type*')
+            return rich.print('[yellow b]*Unknown Type*[/yellow b]')
         if mode.lower() not in ('multi','loop'):
-            return print('*Unknown Mode*')
+            return rich.print('*Unknown Mode*')
         
         if mode.lower() == 'multi':
             self.multiMode[type.lower()] = True if self.multiMode[type.lower()] != True else False
-            print(f"{self.multiMode[type.lower()]} ", end='')
+            rich.print(f"{self.multiMode[type.lower()]} ", end='')
         else:
             self.loopMode[type.lower()] = True if self.loopMode[type.lower()] != True else False
-            print(f"{self.loopMode[type.lower()]} ", end='')
-        print("*Done*")
+            rich.print(f"{self.loopMode[type.lower()]} ", end='')
+        rich.print("*Done*")
     
     def play(self, type:str, item:str):
-        print(f"[AudioManager] Play: ({type}) <{item}> ", end='')
+        rich.print(f"[AudioManager] [blue b]Play:[/blue b] ({type}) [magenta b]<{item}>[/magenta b] ", end='')
         
         looping = int(((2**32) / 2) - 1) if self.loopMode[type.lower()] else 1
         
         # Check if type exist in the list
         if type.lower() not in ('audio','sound'):
-            return print("*Unknown Type*")
+            return rich.print("[yellow b]*Unknown Type*[/yellow b]")
         # Check if the audio actually exist in audioIndex
         audioName = self.audioIndex[type.lower()].get(item)
         if not audioName:
-            return print(f'*Not Found*')
+            return rich.print(f'[red b]*Not Found*[/red b]')
         
         # if it's not empty, something is already playing, remove it and proceed
         # else add to pool
@@ -145,7 +149,8 @@ class AudioManager():
                 return
             pool = self.audioPool['sound']
             pool.remove(sound)
-            
+        
+        # shorten
         def _playAudioMedia(poolItem:AudioMedia, Source:QUrl, volume:int, loops:int):
             poolItem.setSource(Source)
             poolItem.setLoops(loops)
@@ -153,7 +158,7 @@ class AudioManager():
             poolItem.play()
             
         if type.lower() == 'audio':
-            if self.multiMode['audio'] == True:
+            if self.multiMode['audio']: # if true'
                 for poolItem in self.audioPool['audio']:
                     
                     # Skip if it's not EndOfMedia | NoMedia
@@ -171,34 +176,45 @@ class AudioManager():
                     #
                     # if it's EndOfMedia | NoMedia AND StoppedState
                     _playAudioMedia(poolItem, QUrl.fromLocalFile(self.audioIndex['audio'].get(item)), self.settings['volume']['audio']/100, looping)
+                    rich.print('*Done*')
                     return
                 else:
                     if self.rollingPoolIndex == len(self.audioPool['audio']):
                         self.rollingPoolIndex = 0
-                    print(f"<reusing> {self.audioPool['audio'][self.rollingPoolIndex]} ", end='')
+                    rich.print(f"[yellow b]<reusing> {self.audioPool['audio'][self.rollingPoolIndex]}[/yellow b] ", end='')
                     rollingIndex = self.audioPool['audio'][self.rollingPoolIndex]
                     _playAudioMedia(rollingIndex, QUrl.fromLocalFile(self.audioIndex['audio'].get(item)), self.settings['volume']['audio']/100, looping)
                     self.rollingPoolIndex += 1
             else:
                 poolItem = self.audioPool['audio'][0]
                 _playAudioMedia(poolItem, QUrl.fromLocalFile(self.audioIndex['audio'].get(item)), self.settings['volume']['audio']/100, looping)
-            print('*Done*')
+            rich.print('*Done*')
         else:
-            # create new instance of SoundEffect
-            _ = SoundEffect(self.audioIndex['sound'].get(item), self.settings['device'], self.settings['volume'], looping)
+            if self.multiMode['sound']:
+                # create new instance of SoundEffect
+                _ = SoundEffect(self.audioIndex['sound'].get(item), self.settings['device'], self.settings['volume']['sound'], looping)
 
-            # add to pool and delete instance once audio finishes
-            self.audioPool['sound'].append(_)
-            _.playingChanged.connect(lambda: _vanish(_))
-            print("*Done*")
+                # add to pool and delete instance once audio finishes
+                self.audioPool['sound'].append(_)
+                _.playingChanged.connect(lambda: _vanish(_))
+                rich.print("*Done*")
+            else:
+                _ = SoundEffect(self.audioIndex['sound'].get(item), self.settings['device'], self.settings['volume']['sound'], looping)
+                if self.audioPool['sound'] != []:
+                    self.audioPool['sound'][0].stop()
+                    self.audioPool['sound'].append(_)
+                else:
+                    self.audioPool['sound'].append(_)
+                _.playingChanged.connect(lambda: _vanish(_))
+                rich.print("*Done*")
     
     def stopAll(self, type:str):
         if type.lower() not in ('audio','sound'):
-            return print("*Unknown Type*")
+            return rich.print("[yellow b]*Unknown Type*[/yellow b]")
         if type.lower() == 'sound':
             # Brute Force method of stopping all SoundEffect. since if .stop is called, .playingChanged triggers which removes itself
             while self.audioPool.get('sound') != []:
-                print(f'[AudioManager] Stop: {self.audioPool.get('sound')[0]}')
+                rich.print(f'[AudioManager] Stop: {self.audioPool.get('sound')[0]}')
                 
                 # these 3 lines makes no sense to me but it works
                 self.audioPool.get('sound')[0].play() if self.audioPool.get('sound')[0].keptAlive == True else ''
@@ -210,17 +226,17 @@ class AudioManager():
             for each in self.audioPool.get('audio'):
                 if each.mediaStatus() == QMediaPlayer.MediaStatus.NoMedia:
                     continue
-                print(f'[AudioManager] Stop: {each}')
+                rich.print(f'[AudioManager] [red b]Stop: {each}[/red b]')
                 each.stop()
-                each.setSource(QUrl(QUrl.fromLocalFile(None)))
+                each.setSource(QUrl.fromLocalFile(None))
                 
     def resumeAll(self, type:str):
         if type.lower() not in ('audio','sound'):
-            return print("*Unknown Type*")
+            return rich.print("[yellow b]*Unknown Type*[/yellow b]")
         # Brute Force method of stopping all SoundEffect. since if .stop iscalled, .playingChanged triggers which removes itself
         if type.lower() == 'sound':
             for each in self.audioPool['sound']:
-                print(f'[AudioManager] Replay:{each}')
+                rich.print(f'[AudioManager] Replay:{each}')
                 # play it, which will trigger .playingChanged, but... Paused is TRUE so it returns! _vanish() not triggered
                 # this line makes no sense to me why i needa check keptAlive state but it works
                 each.play() if each.keptAlive == True else ''
@@ -231,23 +247,23 @@ class AudioManager():
             for each in self.audioPool.get('audio'):
                 if each.playbackState() != QMediaPlayer.PlaybackState.PausedState:
                     continue
-                print(f'[AudioManager] Resume: {each}')
+                rich.print(f'[AudioManager] [cyan b]Resume: {each}[/cyan b]')
                 each.play()
                 
     
     def pauseAll(self, type:str):
         if type.lower() not in ('audio','sound'):
-            return print("*Unknown Type*")
+            return rich.print("[yellow b]*Unknown Type*[/yellow b]")
         if type.lower() == 'sound':
             for each in self.audioPool['sound']:
-                print(f'[AudioManager] Halt: {each}')
+                rich.print(f'[AudioManager] Halt: {each}')
                 each.keptAlive = True
                 each.stop()
         else:
             for each in self.audioPool.get('audio'):
                 if each.playbackState() != QMediaPlayer.PlaybackState.PlayingState:
                     continue
-                print(f'[AudioManager] Pause: {each}')
+                rich.print(f'[AudioManager] [yellow b]Pause: {each}[/yellow b]')
                 each.pause()
             
 
@@ -266,6 +282,12 @@ class SoundEffect(QSoundEffect):
     def __repr__(self) -> str:
         return f"{self.name}{' (looped)' if self.loopCount() > 1 else ''}"
 
+###
+#  Plan to add PlaybackRate. maybe slowmo, or fast-mo functions.
+###
+#  Plan to add a slight delay when playing and unloading Media,
+#  in the hopes that it would reduce or eliminate crackles when playing audio
+###
 class AudioMedia(QMediaPlayer):
     def __init__(self, count, device:QAudioDevice):
         super().__init__()
--- Soundboard_Backend_PyQt6.py
+++ Soundboard_Backend_PyQt6.py
@@ -1,7 +1,12 @@
-# Looks like PyQt6 has some sound capabilities, might re-write Soundboard_Backend to be fully PyQt
-import time, os, json, xpfpath
-import AudioSystem_PyQt6 as AS_PYQT6
+import time, os, json, xpfpath, rich
 from PyQt6.QtMultimedia import QMediaDevices
+from rich import pretty
+pretty.install()
+
+# Use standard
+if os.name=='nt': import AudioSystem_PyQt6 as AS_PYQT6 
+# Use Linux Pipewire fix
+else: import AudSys_LinuxPatch as AS_PYQT6
 
 ComDispName = []
 LoopTextState, LoopState,  = "  Looping Disabled", 0
@@ -16,11 +21,11 @@ def InitializeSettings():
             with open('Settings.json','r') as SettingsValue:
                 Settings = json.loads(SettingsValue.read())
         except Exception as Err:
-            print(f"\n[PySoundboard] Error: {Err}\n")
+            rich.print(f"\n[PySoundboard] Error: {Err}\n")
             os.remove("Settings.json")
-            print("[PySoundboard] settings.json is being reset")
+            rich.print("[PySoundboard] settings.json is being reset")
             InitializeSettings()
-            print("[PySoundboard] settings.json reset complete")
+            rich.print("[PySoundboard] settings.json reset complete")
     else:
         x = {"AudioDevice":None,"Volume":10,"MaxRows":"8","Splash":"1"}
         with open("Settings.json","a") as DefaultSettingsDump:
@@ -29,12 +34,11 @@ def InitializeSettings():
         
 def InitializeAudioSystem():
     if Settings['AudioDevice'] is None:
-        print('\n[PySoundboard] VB-Audio VoiceMeeter/VB-Audio Virtual Cable [NOT FOUND]\n[PySoundboard] Using [System Default Output] !\n[PySoundboard] <Settings.json> "AudioDevice":None !\n') if os.name == 'nt' else print('\n[PySoundboard] Using [System Default Output] !\n[PySoundboard] <Settings.json> "AudioDevice":None !\n')     
+        rich.print('\n[PySoundboard] VB-Audio VoiceMeeter/VB-Audio Virtual Cable [NOT FOUND]\n[PySoundboard] Using [System Default Output] !\n[PySoundboard] <Settings.json> "AudioDevice":None !\n') if os.name == 'nt' else rich.print('\n[PySoundboard] Using [System Default Output] !\n[PySoundboard] <Settings.json> "AudioDevice":None !\n')     
     def _getDevice():
         for device in QMediaDevices.audioOutputs():
             if device.description() == Settings['AudioDevice']:
                 return device
-    # need implementation for a fallback and actually using the device specified in the settings.json
     return AS_PYQT6.AudioManager(_getDevice(), Settings['Volume'])
     
 def ToggleLoop():
@@ -59,7 +63,7 @@ def ToggleSpamming():
 def GenerateSoundIndex(path) -> tuple:
     AudioFilesIndex:list = []
     SubFoldersIndex:list = []
-    print(f'Scanning [{path}]')
+    rich.print(f'[PySoundboard] Scanning [{path}]')
     try:
         FolderContents = os.scandir(path)
     except:
@@ -69,7 +73,7 @@ def GenerateSoundIndex(path) -> tuple:
         name:str = str(_.name.rsplit(".",1)[0]) # omit file extension.
         folder:str = str(_.path).rsplit(f"{'\\' if os.name=='nt' else '/'}",2)[1] # Get actual folder where file is located.
         AudioFilesIndex.append([folder,name,SoundFile(Entry.path).Play])
-        print(f"[GUI] [Tab: {folder}] (Button: {name})")
+        rich.print(f"[GUI] [blue][Tab: {folder}][/blue] [cyan](Button: {name})[/cyan]")
     # scan "Root" ./SoundFiles Folder for files and Folders
     for Entry in FolderContents:
         add(Entry) if Entry.is_file() else SubFoldersIndex.append(Entry.path)
@@ -83,14 +87,20 @@ def GenerateSoundIndex(path) -> tuple:
 # PyQt Sound System
 class SoundFile:
     def __init__(self, filepath:str):
-        # super().__init__()
         self.file = filepath
-        AudioSystem.load('audio',self.file)
+        self.audioName = ''
+        if  AudioSystem.audioIndex['audio'].get(os.path.splitext(os.path.basename(self.file))[0]):
+            self.audioName = (os.path.splitext(os.path.basename(self.file))[0])
+            self.audioName = f"{len(AudioSystem.audioIndex['audio'])^len(self.audioName)}_{self.audioName}"
+            AudioSystem.load('audio',self.file)
+            rich.print(f'[GUI] [cyan][Button] set to <{self.audioName}>[/cyan]')
+        else:
+            AudioSystem.load('audio',self.file)
     def Play(self):
         global Title
         Title = f"'{self.file}'"
-        AudioSystem.play('audio',os.path.splitext(os.path.basename(self.file))[0])
-        print(f" - {LoopState}/{SpammingState}"+LoopTextState+"/"+SpammingTextState)
+        AudioSystem.play('audio',os.path.splitext(os.path.basename(self.file if self.audioName == '' else self.audioName))[0])
+        rich.print(f" - {LoopState}/{SpammingState}"+LoopTextState+"/"+SpammingTextState)
     def __repr__(self):
         return self.file
 
